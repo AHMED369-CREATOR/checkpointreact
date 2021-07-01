13ca10b9b94141b20c9318db189810a901f832b8
as766 <ahmedjamine@gmail.com>
2021-07-01 15:00:47 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import First, {Fourth } from "./components/First";
+import { Second,Third } from "./components/Second";
+function App() {
+  return (
+    <div className="App">
+<First/>
+<Second/>
+<Third/>
+<Fourth/>
+    </div>
+  );
+}
+
+export default App;
